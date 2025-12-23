4363a1b79e2b78234fcd4e317d61e2d1f5563973
Louis Bourne <chifury@proton.me>
2025-12-23 17:41:38 +0700

    fix: used new api key for model.

--- config/initializers/ruby_llm.rb
+++ config/initializers/ruby_llm.rb
@@ -1,5 +1,6 @@
 RubyLLM.configure do |config|
-  config.openai_api_key = ENV['OPENAI_API_KEY'] || Rails.application.credentials.dig(:openai_api_key)
+  config.openai_api_key = ENV['GITHUB_TOKEN'] || Rails.application.credentials.dig(:openai_api_key)
+  config.openai_api_base = "https://models.inference.ai.azure.com"
   # config.default_model = "gpt-4.1-nano"
 
   # Use the new association-based acts_as API (recommended)
