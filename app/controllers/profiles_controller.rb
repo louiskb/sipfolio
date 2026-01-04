010a79b4b642a2d4718da04d0d39dbc64d721158
Louis Bourne <chifury@proton.me>
2026-01-04 23:28:22 +0700

    feat: sorted the user profiles alphabetically in the profiles controller.

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,7 +1,9 @@
 class ProfilesController < ApplicationController
 
   def index
-    @profiles = Profile.all
+    # joins(:user) adds INNER JOIN users ON users.id = profiles.user_id
+    # users.username references the joined table's column
+    @profiles = Profile.joins(:user).order("users.username ASC")
   end
 
   def show
