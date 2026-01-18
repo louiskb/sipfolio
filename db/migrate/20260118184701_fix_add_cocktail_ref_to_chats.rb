d1500e54d113fb88104318145233158a964f4b68
Louis Bourne <chifury@proton.me>
2026-01-19 02:27:20 +0700

    style: udpated linting in latest migration file fix_add_cocktail_red_to_chats.rb

--- db/migrate/20260118184701_fix_add_cocktail_ref_to_chats.rb
+++ db/migrate/20260118184701_fix_add_cocktail_ref_to_chats.rb
@@ -1,13 +1,13 @@
 class FixAddCocktailRefToChats < ActiveRecord::Migration[7.1]
-def up
-  add_reference :chats, :cocktail, index: true, foreign_key: true
+  def up
+    add_reference :chats, :cocktail, index: true, foreign_key: true
 
-  execute <<-SQL.squish
-    UPDATE chats
-    SET cocktail_id = (SELECT id FROM cocktails ORDER BY id LIMIT 1)
-    WHERE cocktail_id IS NULL;
-  SQL
+    execute <<-SQL.squish
+      UPDATE chats
+      SET cocktail_id = (SELECT id FROM cocktails ORDER BY id LIMIT 1)
+      WHERE cocktail_id IS NULL;
+    SQL
 
-  change_column_null :chats, :cocktail_id, false
-end
+    change_column_null :chats, :cocktail_id, false
+  end
 end
