4d89705abcbefd7e986845cc6bf2f103bb11f82c
Louis Bourne <chifury@proton.me>
2026-01-28 03:33:25 +0700

    feat: add conditional logic to show the appropiate cocktail image whether through Active Storage or pre-selected with SipSense Mix AI.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,10 @@
 module ApplicationHelper
+  # Makes the class attribute value dynamic (adding and removing `active` class) depending on the current page.
   def nav_link_class(path = "#")
     current_page?(path) ? "nav-link active fs-5" : "nav-link fs-5"
   end
 
+  # Renders markdown into HTML with settings for the `redcarpet` gem that parses markdown into HTML.
   def render_markdown(text)
     return "" if text.blank?
 
@@ -18,4 +20,14 @@ module ApplicationHelper
 
     markdown.render(text)
   end
+
+  # Checks whether the cocktail image is either (1) uploaded by the user (via Active Storage) or (2) pre-selected by SipSense Mix AI (pre-saved cocktail images in the `assets/images` path).
+  def cocktail_background_image(cocktail)
+    if cocktail.photo.attached?
+      url_for(cocktail.photo)
+    else
+      asset_path(cocktail.img_url)
+    end
+  end
+  
 end
