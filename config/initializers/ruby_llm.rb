881d07289b13ae1764483f55be3d1a34314e2f47
Louis Bourne <chifury@proton.me>
2026-03-12 10:42:44 +0700

    feat: update explanation on rails credentials.

--- config/initializers/ruby_llm.rb
+++ config/initializers/ruby_llm.rb
@@ -1,5 +1,5 @@
 RubyLLM.configure do |config|
-  config.openai_api_key = ENV.fetch('GITHUB_TOKEN', nil) || ENV.fetch('OPENAI_API_KEY', nil) # Rails.application.credentials.dig(:openai_api_key) if using Rails credentials
+  config.openai_api_key = ENV.fetch('GITHUB_TOKEN', nil) || ENV.fetch('OPENAI_API_KEY', nil) # Add `Rails.application.credentials.dig(:openai_api_key)` if using Rails credentials. FYI Run `rails credentials:edit` to open and decrypt YAML file where you'd add and edit credentials e.g. `openai_api_key: your-api-key-here`. It's decrypted at runtime using a master key `config/master.key`, which is never committed to Git. Rails credentials work well in development without needing a `.env` file.
   config.openai_api_base = "https://models.inference.ai.azure.com"
   # config.default_model = "gpt-4.1-nano"
 
